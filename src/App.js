e61e8895032b6b33b7041e69a950d42a1163c704
Ravi Ahuja <ravi.ahuja@medisecure.com.au>
2023-01-17 09:36:22 +1100

    Counter Slice redux global state for counter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Counter from './features/counter/Counter';
+
+function App() {
+  return (
+    <main className='App'>
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
